cfa2a701b229936fb305b6bf645b6b3d940c9e91
latifa212121 <latitursunova@gmail.com>
2021-07-15 20:16:52 +0500

    abc

---
+++ script.js
@@ -0,0 +1,208 @@
+
+// let a = prompt()
+
+
+// console.log(a > 10 ? 'ок' : 'не ок')
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+// let idx = arr.indexOf(4)
+
+// arr.splice(idx, 1)
+// console.log(arr)
+// let arr =  [ 1, 2, 3, 4, 5, 6, 7, 82734, 859,34, 12, 34]
+// let arr_str = ['Alex', 'Barbara', 'John', 'Michael']
+// let arr_filtered = arr.filter(item => item <=10)
+// let arr_str_filtered = arr_str.filter(item => item[0]== 'A')
+// console.log(arr_str_filtered)
+
+// //ОБЪЕКТЫ
+// let me = {
+//     name:'Latifa',
+//     age: 15,
+//     isMarried: false,
+//     hobbies: [ 'JS' , 'book',  ]
+// }
+
+// // console.log(me).
+// let arr = [
+//     14,
+//     {
+//         a: [
+//             {
+//                 a: true,
+//                 b: false,
+//                 c: [
+//                     {
+//                         a: 'ME'
+//                     }
+//                 ]
+//             }
+//         ]
+//     },
+//     28,
+//     {
+//     a: {
+//         a: [1, 12, 31, 23, 12, 'IT', 3123]
+//     }
+// }, 1, 2, 3, { a: { a: { a: ['IS'] } } }]
+// let arr_2 = [{
+
+// }, 1, { b: [['HELLO'],{ a: 'WORLD' }]}, 3]
+
+// console.log( arr_2[0].)
+
+// let arr = []
+
+// 1. Добавить в массив цифру 10
+// 2. Добавить в конец цифру 12
+// 3. Добавить в начало сумму двух последних элементов
+// 4. Удалить второй элемент
+// 5. Поменять второй элемент на цифру 15
+// let arr = []
+
+// 1. Добавить в конец массива цифру 10
+// 2. Добавить в начало массива последний элемент, умноженный на 2
+// 3. Добавить в середину массива объект с ключом a, который равен первому элементу массива
+// 4. Удалить третий элемент массива
+// 5. Добавить в объект ключ b, который равен всему масс
+
+// let arr = []
+// arr.push(10)
+// arr.unshift(12)
+// arr.unshift(arr[0] + arr[1])
+// arr.slice(0,1)
+// arr[1] = 15
+// console.log(arr)
+// let arr = []
+// let arr_2 = []
+
+// 1. Добавить в arr пустой объект
+// 2. Добавить в arr_2 массив arr
+// 3. Добавить ключ a в объект массива arr, который равен массиву arr_2
+// 4. Добавить true в конец массива arr
+// 5. Добавить prompt('Ваше имя') в конец массива arr_2
+
+
+// 1. Добавить в массив каждую букву str отдельно
+// 2. Удалить третий элемент массива
+// 3. Добавить в конец последний три буквы str
+// 4. Добавить в начало confirm
+// 5. Удалить первый и последний элементы
+// 6. Добавить в начало 'HE' и 'LO' (CAPS LOCK)
+// 7. Добавить в конец массив arr первый элемент arr
+
+
+// let arr = []
+// let arr_2 = []
+// arr[0]={}
+// arr_2 = arr
+// arr[0].a = arr_2
+// arr.push(true)
+// arr_2.push(prompt('Ваше имя'))
+// // console.log(arr)
+// let arr = []
+// let str = 'Hello'
+// arr[0] = str.slice(0,1)
+// arr[1]=str.slice(1,2)
+// arr[2] = str.slice(2,3)
+// arr[3] = str.slice(3,4)
+// arr[4] = str.slice(4,5)
+// arr.splice(2,1)
+// arr.push(arr.splice(2,3))
+// arr.unshift(confirm)
+// arr.splice(-1,0)
+// arr.unshift()
+// console.log(arr)
+
+
+// //Циклы for
+// let arr = [ 1, 2 ,3 , 4, 5, 6]
+
+// for( let item of arr){
+//     if (item == 4){
+//         console.log('ERROR')
+//     }
+//     else{}
+
+// let arr = [ 1, 2 ,3 , 4, 5, 6]
+// let i = 0
+// for( let item of arr){
+//     arr[i] +=10
+//     i++
+// }
+// console.log(arr)
+
+// let arr = [ 1, 2 ,3 , 4, 5, 6]
+// for( let item of arr){
+//     arr[ arr.indexOf(item)] += 10
+// }
+// console.log(arr)
+
+// let arr = ['Alex', 'Barbara', 'Michael' ]
+// let str = ''
+
+// for(let item of arr){
+//     str += item.slice(0,1)
+// }
+// console.log(str)
+
+
+// for(let item of arr){
+//     item.price = item.money - (item.money / 100 * item.sale)
+//  }
+
+
+// let total = 0
+// let cashback = 0
+
+// let products = [
+//     {
+//         name: 'Молоко',
+//         pieces: 1,
+//         price: 10000,
+//         sale: 14,
+//         cashback: 4
+//     },
+//     {
+//         name: 'Йогурт',
+//         pieces: 5,
+//         price: 14500,
+//         sale: 7.5,
+//         cashback: 3
+//     },
+//     {
+//         name: 'Шоколад',
+//         pieces: 2,
+//         price: 18000,
+//         sale: 21,
+//         cashback: 6.5
+//     },
+//     {
+//         name: 'Говядина',
+//         pieces: 2,
+//         price: 65000,
+//         sale: 1,
+//         cashback: 0
+//     },
+//     {
+//         name: 'Энергетик',
+//         pieces: 2,
+//         price: 14500,
+//         sale: 2,
+//         cashback: 5
+//     },
+// ]
+// for (let item of products) {
+//     let norm_price = (item.price * (item.pieces / 100 * item.sale))* item.price
+//     total += norm_price
+//     cashback += norm_price / 100 * item.cashback
+// }
+// console.log(total)
+// console.log(cashback)
+
+// //Цикл для обьектов 
+// for (let item in obj){
+//     total+=
+// }
+
+//  работа с githab 
+
